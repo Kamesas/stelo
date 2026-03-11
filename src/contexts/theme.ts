c0cfe6f43be00a85563dae0b8c05a5347a4bb46d
AlexSemak <semakaleksandr2014@gmail.com>
2026-03-11 14:28:09 +0200

    refactor: theme config

--- src/contexts/theme.ts
+++ src/contexts/theme.ts
@@ -1,40 +1,17 @@
 import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react";
+import { colors } from "./theme.colors";
+import { fonts, textStyles } from "./theme.typography";
 
 defaultConfig.theme!.tokens!.colors = {};
+defaultConfig.theme!.tokens!.fonts = {};
+defaultConfig.theme!.textStyles = {};
 
 const config = defineConfig({
   theme: {
+    textStyles,
     tokens: {
-      colors: {
-        s: {
-          plum: { value: "#291928" },
-          tangerine: { value: "#F0B377" },
-
-          tan: { value: "#EBE6E0" },
-          bone: { value: "#F8F6F2" },
-          white: { value: "#FFFFFF" },
-
-          error: { value: "#AC3D2E" },
-          warning: { value: "#FCAF2D" },
-          success: { value: "#015651" },
-
-          dark: {
-            5: { value: "rgba(41, 25, 40, 0.05)" },
-            10: { value: "rgba(41, 25, 40, 0.1)" },
-            40: { value: "rgba(41, 25, 40, 0.4)" },
-            70: { value: "rgba(41, 25, 40, 0.7)" },
-            80: { value: "rgba(41, 25, 40, 0.8)" },
-          },
-
-          light: {
-            10: { value: "rgba(248, 246, 242, 0.1)" },
-            20: { value: "rgba(248, 246, 242, 0.2)" },
-            40: { value: "rgba(248, 246, 242, 0.4)" },
-            60: { value: "rgba(248, 246, 242, 0.6)" },
-            80: { value: "rgba(248, 246, 242, 0.8)" },
-          },
-        },
-      },
+      fonts,
+      colors,
     },
   },
 });
