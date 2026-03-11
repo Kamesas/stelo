6e5e4d7683f1b60ba14968dd97792f8d22106ff6
AlexSemak <semakaleksandr2014@gmail.com>
2026-03-11 14:27:33 +0200

    feat: add colors theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import { Box } from "@chakra-ui/react";
 export default function Home() {
   return (
     <div>
-      <Box bg="plum">Box example</Box>
+      <Box bg="s.tangerine">Box example</Box>
+      <Box bg="dark.40">Box example</Box>
+      <Box bg="light.20">Box example</Box>
+      <Box bg="tangerine">Box example</Box>
+      <Box bg="s.plum">Box example</Box>
     </div>
   );
 }
